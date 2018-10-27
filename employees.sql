42d7452eae492fd6254087a485c9769c9966c8da
Shaun Carter <thereal.shauncarter@gmail.com>
2018-10-27 15:06:43 -0500

    populated seed file for employment database.

---
+++ employees.sql
@@ -0,0 +1,8 @@
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    age INT NOT NULL,
+    current_status VARCHAR(255) NOT NULL DEFAULT 'employed'
+);
