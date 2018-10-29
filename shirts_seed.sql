2ced536cea017025bdfe30ae32198c977bcd1564
Shaun Carter <thereal.shauncarter@gmail.com>
2018-10-29 12:56:50 -0600

    Adding my solutions to book database challenge.

--- shirts_seed.sql
+++ shirts_seed.sql
@@ -1,5 +1,6 @@
 INSERT INTO shirts (article, color, shirt_size, last_worn)
-VALUES('t-shirt', 'white', 'S', 10),
+VALUES
+('t-shirt', 'white', 'S', 10),
 ('t-shirt', 'green', 'S', 200),
 ('polo shirt', 'black', 'M', 10),
 ('tank top', 'blue', 'S', 50),
