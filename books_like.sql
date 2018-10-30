65b892e8179682c951518167e28fb37912ea1c24
Shaun Carter <thereal.shauncarter@gmail.com>
2018-10-30 12:30:24 -0600

    adding like practice for unique characters for books table.

--- books_like.sql
+++ books_like.sql
@@ -17,3 +17,27 @@ WHERE  title LIKE '%the';
 SELECT title
 FROM books
 WHERE title LIKE '%the%';
+
+SELECT title, stock_quantity
+FROM books;
+
+SELECT title, stock_quantity
+FROM books
+WHERE stock_quantity LIKE '____';
+
+SELECT title, stock_quantity
+FROM books
+WHERE stock_quantity LIKE '__';
+
+(235)234-0987 LIKE '(___)___-____'
+
+SELECT title
+FROM books;
+
+SELECT title
+FROM books
+WHERE title LIKE '%\%%'
+
+SELECT title
+FROM books
+WHERE title LIKE '%\_%'
