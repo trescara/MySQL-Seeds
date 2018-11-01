f07e84bda22d7f963d4b8f58fb9e2f83b3839051
Shaun Carter <thereal.shauncarter@gmail.com>
2018-11-01 13:17:42 -0600

    adding online solutions to functions file.

--- books_logicalop.sql
+++ books_logicalop.sql
@@ -365,4 +365,42 @@ SELECT author_lname,
 FROM books
 GROUP BY author_lname, author_fname;
 
+-- ONLINE SOLUTIONS
+-- SELECT
+--   title,
+--   author_lname
+-- FROM books
+-- WHERE
+--     author_lname LIKE 'C%' OR
+--   author_lname LIKE 'S%';
+
+-- SELECT
+--   title,
+--   author_lname
+-- FROM books
+-- WHERE
+--     SUBSTR(author_lname,1,1) = 'C' OR
+--   SUBSTR(author_lname,1,1) = 'S';
+
+-- SELECT title, author_lname
+-- FROM books
+-- WHERE SUBSTR(author_lname,1,1) IN ('C', 'S');
+
+-- SELECT
+--   title,
+--   author_lname,
+--   CASE
+--         WHEN title LIKE '%stories%' THEN 'Short Stories'
+--         WHEN title = 'Just Kids' OR title = 'A Heartbreaking Work of Staggering Genius' THEN 'Memoir'
+--         ELSE 'Novel'
+--     END AS TYPE
+-- FROM books;
+
+-- SELECT author_fname, author_lname,
+--   CASE
+--         WHEN COUNT(*) = 1 THEN '1 book'
+--         ELSE CONCAT(COUNT(*), ' books')
+--     END AS COUNT
+-- FROM books
+-- GROUP BY author_lname, author_fname;
 
